85c1e8dabfd5141f13b94ae8b85b1bdee3fb0e49
kruall <kru.bash.all@gmail.com>
2022-07-19 23:28:55 +0000

    refactor log

--- src/core/log.hpp
+++ src/core/log.hpp
@@ -7,117 +7,76 @@
 
 namespace home_task::log {
 
+template <bool ... _Flags>
+struct Logger {
+    template <typename ... _Args>
+    static void Write(_Args&&... _args) {
+        if ((false || ... || _Flags)) {
+            std::ostringstream sout;
+            (sout << ... << std::forward<_Args>(_args)) << std::endl;
+            std::cerr << sout.str();
+        }
+    }
+};
+
+
 template <typename ... _Args>
 void Write(_Args&&... _args) {
-    if (!magic_numbers::WithLog) {
-        return;
-    }
-    std::ostringstream sout;
-    (sout << ... << std::forward<_Args>(_args)) << std::endl;
-    std::cerr << sout.str();
+    Logger<magic_numbers::WithLog>::Write(std::forward<_Args>(_args)...);
 }
 
 template <typename ... _Args>
 void WriteMutex(_Args&&... _args) {
-    if (!magic_numbers::WithLog || !magic_numbers::WithMutexLog || !magic_numbers::WithNetworkLog) {
-        return;
-    }
-    std::ostringstream sout;
-    (sout << ... << std::forward<_Args>(_args)) << std::endl;
-    std::cerr << sout.str();
+    Logger<magic_numbers::WithMutexLog>::Write(std::forward<_Args>(_args)...);
 }
 
 template <typename ... _Args>
 void WriteNetwork(_Args&&... _args) {
-    if (!magic_numbers::WithLog || !magic_numbers::WithNetworkLog) {
-        return;
-    }
-    std::ostringstream sout;
-    (sout << ... << std::forward<_Args>(_args)) << std::endl;
-    std::cerr << sout.str();
+    Logger<magic_numbers::WithNetworkLog>::Write(std::forward<_Args>(_args)...);
 }
 
 
 template <typename ... _Args>
 void WriteState(_Args&&... _args) {
-    if (!magic_numbers::WithLog || !magic_numbers::WithStateLog) {
-        return;
-    }
-    std::ostringstream sout;
-    (sout << ... << std::forward<_Args>(_args)) << std::endl;
-    std::cerr << sout.str();
+    Logger<magic_numbers::WithStateLog>::Write(std::forward<_Args>(_args)...);
 }
 
 template <typename ... _Args>
 void WriteServerState(_Args&&... _args) {
-    if (!magic_numbers::WithLog || !magic_numbers::WithStateLog || !magic_numbers::WithServerStateLog) {
-        return;
-    }
-    std::ostringstream sout;
-    (sout << ... << std::forward<_Args>(_args)) << std::endl;
-    std::cerr << sout.str();
+    Logger<magic_numbers::WithStateLog, magic_numbers::WithServerStateLog>::Write(std::forward<_Args>(_args)...);
 }
 
 template <typename ... _Args>
 void WriteClientState(_Args&&... _args) {
-    if (!magic_numbers::WithLog || !magic_numbers::WithStateLog || !magic_numbers::WithClientStateLog) {
-        return;
-    }
-    std::ostringstream sout;
-    (sout << ... << std::forward<_Args>(_args)) << std::endl;
-    std::cerr << sout.str();
+    Logger<magic_numbers::WithStateLog, magic_numbers::WithClientStateLog>::Write(std::forward<_Args>(_args)...);
 }
 
 template <typename ... _Args>
 void WriteFastClientState(_Args&&... _args) {
-    if (!magic_numbers::WithLog || !magic_numbers::WithStateLog || !magic_numbers::WithFastClientStateLog) {
-        return;
-    }
-    std::ostringstream sout;
-    (sout << ... << std::forward<_Args>(_args)) << std::endl;
-    std::cerr << sout.str();
+    Logger<magic_numbers::WithStateLog, magic_numbers::WithClientStateLog, magic_numbers::WithFastClientStateLog>
+        ::Write(std::forward<_Args>(_args)...);
 }
 
 template <typename ... _Args>
 void WriteDestructor(_Args&&... _args) {
-    if (!magic_numbers::WithLog || !magic_numbers::WithDestructorLog) {
-        return;
-    }
-    std::ostringstream sout;
-    (sout << ... << std::forward<_Args>(_args)) << std::endl;
-    std::cerr << sout.str();
+    Logger<magic_numbers::WithDestructorLog>::Write(std::forward<_Args>(_args)...);
 }
 
 
 template <typename ... _Args>
 void WriteClientRunner(_Args&&... _args) {
-    if (!magic_numbers::WithLog || !magic_numbers::WithRunnerLog || !magic_numbers::WithClientRunnerLog) {
-        return;
-    }
-    std::ostringstream sout;
-    (sout << ... << std::forward<_Args>(_args)) << std::endl;
-    std::cerr << sout.str();
+    Logger<magic_numbers::WithRunnerLog, magic_numbers::WithClientRunnerLog>::Write(std::forward<_Args>(_args)...);
 }
 
 
 template <typename ... _Args>
 void WriteServerRunner(_Args&&... _args) {
-    if (!magic_numbers::WithLog || !magic_numbers::WithRunnerLog || !magic_numbers::WithServerRunnerLog) {
-        return;
-    }
-    std::ostringstream sout;
-    (sout << ... << std::forward<_Args>(_args)) << std::endl;
-    std::cerr << sout.str();
+    Logger<magic_numbers::WithRunnerLog, magic_numbers::WithServerRunnerLog>::Write(std::forward<_Args>(_args)...);
 }
 
 template <typename ... _Args>
 void WriteDecardTree(_Args&&... _args) {
-    if (!magic_numbers::WithLog || !magic_numbers::WithDecardTreeLog) {
-        return;
-    }
-    std::ostringstream sout;
-    (sout << ... << std::forward<_Args>(_args)) << std::endl;
-    std::cerr << sout.str();
+    Logger<magic_numbers::WithDecardTreeLog>::Write(std::forward<_Args>(_args)...);
 }
 
 }
